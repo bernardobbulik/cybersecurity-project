8717846d445d88fa03ce7a7449cecc0bd8d27656
Bernardo Bulik <bernardobulik@gmail.com>
2026-06-17 13:53:49 -0300

    js no contato

---
+++ js/contato.js
@@ -0,0 +1,12 @@
+function submitContact() {
+  if (
+    document.getElementById("nome").value == "" ||
+    document.getElementById("email").value == "" ||
+    document.getElementById("assunto").value == ""
+  ) {
+    alert("Preencha todos os campos para continuar!");
+    return;
+  }
+  alert("Página em construção!");
+  return;
+}
